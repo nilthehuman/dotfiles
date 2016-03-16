cfe25de4990fafa8ae10cfc837734dc05344c333
Dániel Arató <nil.the.human@gmail.com>
2016-03-16 13:15:31 +0100

    Remove whitespace from assignment (revert c0a2756)

--- install.sh
+++ install.sh
@@ -25,14 +25,14 @@ declare -a linkdirs=(
     "${HOME}/.vim"
 )
 declare -A links=(
-["${DOTFILESDIR}/bash/bash_aliases"] = "${HOME}/.bash_aliases"
-["${DOTFILESDIR}/bash/bashrc"]       = "${HOME}/.bashrc"
-["${DOTFILESDIR}/ghc/ghci.conf"]     = "${HOME}/.ghc/ghci.conf"
-["${DOTFILESDIR}/git/gitconfig"]     = "${HOME}/.gitconfig"
-["${DOTFILESDIR}/openbox/rc.xml"]    = "${HOME}/.config/openbox/rc.xml"
-["${DOTFILESDIR}/openbox/autostart"] = "${HOME}/.config/openbox/autostart"
-["${DOTFILESDIR}/terminator/config"] = "${HOME}/.config/terminator/config"
-["${DOTFILESDIR}/vim/vimrc"]         = "${HOME}/.vim/vimrc"
+    ["${DOTFILESDIR}/bash/bash_aliases"]="${HOME}/.bash_aliases"
+    ["${DOTFILESDIR}/bash/bashrc"]="${HOME}/.bashrc"
+    ["${DOTFILESDIR}/ghc/ghci.conf"]="${HOME}/.ghc/ghci.conf"
+    ["${DOTFILESDIR}/git/gitconfig"]="${HOME}/.gitconfig"
+    ["${DOTFILESDIR}/openbox/rc.xml"]="${HOME}/.config/openbox/rc.xml"
+    ["${DOTFILESDIR}/openbox/autostart"]="${HOME}/.config/openbox/autostart"
+    ["${DOTFILESDIR}/terminator/config"]="${HOME}/.config/terminator/config"
+    ["${DOTFILESDIR}/vim/vimrc"]="${HOME}/.vim/vimrc"
 )
 
 for linkdir in "${linkdirs[@]}"; do
